0503b4959dbf2e6c3e1216c495c95c48e3dcf626
kayweba <under4e98@mail.ru>
2020-01-08 19:43:23 +0300

    Первая версия приложения, без рефакторинга

---
+++ js/script.js
@@ -0,0 +1,357 @@
+const dataObject = {
+    "bonuses": [{
+        "title": "Nethouse.ru",
+        "description": "Конструктор сайтов",
+        "link": "https://nethouse.ru/?p=travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Appfollow.io",
+        "description": "SDK",
+        "link": "http://appfollow.io/+travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Semrush.com",
+        "description": "Анализ",
+        "link": "https://www.semrush.com/sem/?ref=2016127405",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "logaster.ru",
+        "description": "Логотип",
+        "link": "https://www.logaster.ru/?utm_source=travelpayouts&utm_medium=Travelpayouts_market_25off&utm_content=banner&utm_campaign=Travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "reg.ru",
+        "description": "SSL",
+        "link": "https://www.reg.ru/ssl-certificate/?utm_source=travel&utm_campaign=travelssl&utm_medium=email",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Seolib.ru ",
+        "description": "Анализ",
+        "link": "https://seolib.ru/info/monitoring_positions?utm_source=aviasales&utm_campaign=marketplace&utm_medium=cabinet&utm_content=bonus",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "lpgenerator.ru",
+        "description": "Лидогенерация",
+        "link": "http://lpgenerator.ru/?partner_id=c1b4cc",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Majordomo.ru",
+        "description": "Хостинг",
+        "link": "https://www.majordomo.ru/?utm_source=travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Setup.ru",
+        "description": "Конструктор сайтов",
+        "link": "http://www.setup.ru/instructions/promocode",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "umi.com",
+        "description": "Конструктор сайтов",
+        "link": "http://umi.ru/affiliate/?partner_id=3728",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "ukit.com",
+        "description": "Конструктор сайтов",
+        "link": " https://ukit.com/ru?utm_source=aviasales&utm_medium=email&utm_campaign=ukit_aviasales",
+        "promocode": "travelpаyouts"
+      },
+      {
+        "title": "topvisor.com",
+        "description": "Аналитика",
+        "link": "https://topvisor.ru/promo/travelpayouts/?utm_source=travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "netology.ru",
+        "link": "http://netology.ru",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "seopult.ru",
+        "description": "Продвижение сайта",
+        "link": "https://promopult.ru/ref/0e279a2772d18ed5",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "sendpulse.com",
+        "description": "Рассылка",
+        "link": "https://sendpulse.com/ru/features/email?coupon=TRVL-PAY-OUTS&utm_source=travelpayouts&utm_medium=referral&utm_campaign=travelpayouts-coupon",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Turbotext.ru",
+        "description": "Биржа контента",
+        "link": "http://turbotext.ru/promo/129/reg/?utm_source=travelpayouts",
+      },
+      {
+        "title": "jivosite.ru",
+        "description": "Чат",
+        "link": "https://www.jivosite.ru/?utm_source=newsletter&utm_medium=email&utm_content=q1&utm_campaign=travelpayouts_gift_14_03_2017",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Witget.com",
+        "description": "Виджет",
+        "link": "https://witget.com/?partner=travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Spywords.ru",
+        "description": "Анализ",
+        "link": "http://spywords.ru?utm_source=travelpayouts&utm_medium=display&utm_campaign=partner_discount",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Etxt.ru",
+        "description": "Биржа контента",
+        "link": "https://www.etxt.ru/?utm_source=promocode&utm_campaign=travelpayouts&utm_medium=cpa&utm_term=300&utm_content=280217",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Roistat.com",
+        "description": "Аналитика",
+        "link": "https://cloud.roistat.com/user/register ",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Serpstat.com",
+        "description": "Анализ",
+        "link": "https://serpstat.com/users/profile/?utm_medium=email&utm_source=travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "HideMy.name",
+        "description": "VPN",
+        "link": "",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "webhost1.ru",
+        "description": "Хостинг",
+        "link": "https://webhost1.ru/?utm_source=travelpayouts",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Mail.Ru",
+        "description": "Облако ",
+        "link": "https://biz.mail.ru/?utm_source=travelpayout ",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "Unisender.com",
+        "description": "Рассылка",
+        "link": "https://www.unisender.com/ru/about/bonusy/bonus15?utm_source=travelpayouts&utm_medium=partners&utm_content=7&utm_campaign=email",
+        "promocode": "travelpayouts"
+      },
+      {
+        "title": "getuniq.me",
+        "description": "Контекстная реклама",
+        "link": "http://travelpayouts.getuniq.me",
+        "promocode": "travelpayouts"
+      }
+    ],
+    "header": {
+      "balance": 213920,
+      "next_payout": 159465,
+      "currency": "rub"
+    }
+  }
+
+let servicesTab = document.querySelector("#services-tab");
+let svg = document.querySelector("#service-svg");
+let container = document.querySelector(".container");
+let whiteSection = document.querySelector('.white-section');
+let services = document.querySelector('.services');
+let jsonData = document.querySelector('.json-data');
+let promocodeContainer = document.querySelector('.promocode-container');
+
+
+servicesTab.onclick = servicesTabClick;
+function servicesTabClick() {
+    if (svg.style !== "#fff") {
+        svg.style.fill = "#fff";
+    }
+    whiteSection.style.opacity = '1';
+    whiteSection.classList.add("animated", "fadeIn");
+
+    // Передаем данные из массива dataObject
+
+    if(!promocodeContainer.classList.contains('active')) {
+      servicesTitle("Сервисы");
+      filter();
+      filterInput();
+      viewData();
+      searchPromocode();
+      viewBalance(dataObject.header.balance, dataObject.header.next_payout);
+    } 
+
+
+}
+
+function viewBalance(balance, payout) {
+    container.innerHTML = '';
+
+    let balanceDiv = document.createElement("div");
+    balanceDiv.classList.add("balance-div", "animated", "fadeIn");
+    balanceDiv.textContent += "Баланс";
+
+    let balanceP = document.createElement("p");
+    balanceP.classList.add("balance-p");
+
+
+    let payoutDiv = document.createElement("div");
+    payoutDiv.classList.add("payout-div", "animated", "fadeIn");
+    payoutDiv.textContent += "К выплате";
+
+    let payoutP = document.createElement("p");
+    payoutP.classList.add("payout-p");
+    payoutP.textContent += payout + " " + "₽";
+
+
+    container.appendChild(balanceDiv);
+    balanceDiv.appendChild(balanceP);
+    container.appendChild(payoutDiv);
+    payoutDiv.appendChild(payoutP);
+    balanceP.textContent += balance + " " + "₽";
+
+
+}
+
+function servicesTitle(title) {
+    services.innerHTML = '';
+    let titleName = document.createElement('h2');
+    titleName.classList.add('title', 'animated', 'fadeIn');
+    titleName.textContent = title;
+    services.appendChild(titleName);
+}
+
+function filter() {
+    let filterTitle = document.createElement('p');
+    filterTitle.classList.add('filter');
+    filterTitle.textContent = 'Фильтр';
+    services.appendChild(filterTitle);
+}
+
+function filterInput() {
+    let input = document.createElement('input');
+    input.classList.add('filter-input');
+    services.appendChild(input);
+
+    let btn = document.createElement('button');
+    btn.classList.add('throw-off');
+    btn.textContent = "Сбросить";
+    input.after(btn);
+}
+
+function viewData() {
+    for (let key in dataObject.bonuses) {
+      let title = dataObject.bonuses[key].title;
+      let description = dataObject.bonuses[key].description;
+      let link = dataObject.bonuses[key].link;
+      let promocode = dataObject.bonuses[key].promocode;
+      createPromocode(title, description, link, promocode);
+    }
+}
+
+function createPromocode(title , description, link, promocode) { /*, description, link, promocode*/
+
+    // Создание содержимое для div с классом "promocode"
+    let promocodeDiv = document.createElement('div');
+    let titleAndDescriptionDiv = document.createElement('div');
+    let mainValueDiv = document.createElement('div');
+    let promocodeValueContainer = document.createElement('div');
+    let linkDiv = document.createElement('div');
+
+    // Создание текста
+    let titleP = document.createElement('p');
+    let descriptionP = document.createElement('p');
+    let signaturePromocode = document.createElement('p');
+    signaturePromocode.classList.add('signature')
+    signaturePromocode.textContent = "ПРОМОКОД";
+
+    let promocodeValue = document.createElement('p');
+    promocodeValue.textContent = promocode;
+
+    let copyPicture = document.createElement('img');
+    copyPicture.classList.add('copy-btn');
+    copyPicture.setAttribute('data-clipboard-text', promocodeValue.textContent);
+    copyPicture.src = "img/copy.png";
+
+
+
+    // Кнопка для перехода на сайт
+    let linkBtn = document.createElement('a');
+    linkBtn.classList.add('link');
+    linkBtn.textContent = "Получить бонус";
+    linkBtn.href = link;
+    linkBtn.target = '_blank';
+
+    titleAndDescriptionDiv.classList.add('title-descr');
+    titleAndDescriptionDiv.appendChild(titleP);
+    titleAndDescriptionDiv.appendChild(descriptionP);
+
+    promocodeValueContainer.classList.add('promocode-value-container');
+    promocodeValueContainer.appendChild(promocodeValue);
+    promocodeValueContainer.appendChild(copyPicture);
+
+    // promocodeValueContainer.appendChild(linkBtn);
+    linkDiv.classList.add('link-container')
+    linkDiv.appendChild(linkBtn);
+    mainValueDiv.classList.add('main-promocode-container');
+    mainValueDiv.appendChild(signaturePromocode);
+    mainValueDiv.appendChild(promocodeValueContainer);
+
+
+    // Наполнение текстом заголовка и описания
+    titleP.classList.add('promocode-title');
+    titleP.textContent += title;
+    descriptionP.classList.add('promocode-description');
+    descriptionP.textContent += description;
+
+    // Работа с основным контейнером с промокодом
+    promocodeDiv.classList.add('promocode', 'animated', 'fadeIn');
+    promocodeDiv.appendChild(titleAndDescriptionDiv);
+    promocodeDiv.appendChild(mainValueDiv);
+    promocodeDiv.appendChild(linkDiv);
+    promocodeContainer.appendChild(promocodeDiv);
+    promocodeContainer.classList.add('active');
+
+    let clipboard = new ClipboardJS('.copy-btn');
+}
+
+function searchPromocode() {
+  let resetBtn = document.querySelector('.throw-off');
+  let input = document.querySelector('.filter-input');
+  let promocodes = document.querySelectorAll('.promocode');
+
+  input.oninput = function() {
+    let val = this.value.trim().toLowerCase();
+    if (val != "") {
+      promocodes.forEach(function(elem) {
+        if(elem.innerText.toLowerCase().search(val) == - 1) {
+          elem.classList.add("hide");
+        } else if (val == ""){
+          promocodes.forEach(function(elem) {
+            elem.classList.remove("hide");
+          });
+        }
+      });
+    } 
+    resetBtn.onclick = () => {
+      promocodes.forEach(function(elem) {
+        elem.classList.remove("hide");
+        input.value = '';
+      });
+    }
+  };
+}
